776c866f539c6d5e46f10541783a13cd81dfc1d9
jagennivasan <123072092+jagennivasan@users.noreply.github.com>
2024-04-20 10:04:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,10 +2,9 @@ import ToDoItem from "@/components/ToDoItem";
 
 export default function Home() {
   return (
-    <h1>hello</h1>
-    // <>
-    // <ToDoItem />  
+    <>
+    <ToDoItem />  
     
-    // </>
+    </>
   );
 }
